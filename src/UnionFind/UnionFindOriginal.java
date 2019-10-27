69a8662873467ed7ce0a32cc9b1d848f21baecf7
monpro <monproshen@gmail.com>
2019-10-27 17:17:32 +1100

    use array to implement the union-find

---
+++ src/UnionFind/UnionFindOriginal.java
@@ -0,0 +1,46 @@
+public class UnionFindOriginal implements UF {
+
+    private int[] id;    // is an array
+
+    public UnionFindOriginal(int size) {
+
+        id = new int[size];
+
+        for (int i = 0; i < size; i++)
+            id[i] = i;
+    }
+
+    @Override
+    public int getSize(){
+        return id.length;
+    }
+
+    // O(1)
+    private int find(int p) {
+        if(p < 0 || p >= id.length)
+            throw new IllegalArgumentException("p is out of bound.");
+
+        return id[p];
+    }
+
+    // O(1)
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    // O(n)
+    @Override
+    public void unionElements(int p, int q) {
+
+        int pID = find(p);
+        int qID = find(q);
+
+        if (pID == qID)
+            return;
+
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pID)
+                id[i] = qID;
+    }
+}
